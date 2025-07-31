9f0a064b5d9e50f723d5fb0679da1401f455284e
FValenzuelaB <164669852+FValenzuelaB@users.noreply.github.com>
2025-07-31 01:28:02 +0000

    ejemplo clases

--- mi_app/urls.py
+++ mi_app/urls.py
@@ -1,8 +1,21 @@
 
 from django.urls import path
-from .views import hola,listar_productos
+from .views import (
+    ProductoListView,
+    ProductoCreateView,
+    ProductoDetailView,
+    ProductoUpdateView,
+    ProductoDeleteView,
+    desafio,
+    base
+)
 
 urlpatterns = [
-    path("", hola, name="hola"),
-    path("productos",listar_productos, name="listar_productos")
+    path("",ProductoListView.as_view(), name="lista_productos"),
+    path("create",ProductoCreateView.as_view(), name="create_productos"),
+    path("productos/<int:pk>",ProductoDetailView.as_view(), name="detail_productos"),
+    path("producto/actualizacion/<int:pk>",ProductoUpdateView.as_view(), name="update_productos"),
+    path("producto/delete/<int:pk>",ProductoDeleteView.as_view(), name="delete_productos"),
+    path("desafio/", desafio, name="desafio"),
+    path("base/", base, name="base")
 ]
--- mi_app/views.py
+++ mi_app/views.py
@@ -1,10 +1,56 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from .models import Producto
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 # Create your views here.
-def hola(request):
-    return render(request, "hola.html")
+def base(request):
+    return render(request,"base.html",{})
 
 def listar_productos(request):
     productos=Producto.objects.all()
     return render(request,"lista_de_productos.html", {"productos":productos})
+
+#===C.R.U.D PARA PRODUCTOS===
+class ProductoListView(ListView):
+    model = Producto
+    template_name = "productos/producto_list.html"
+    context_object_name = "productos"
+
+class ProductoDetailView(DetailView):
+    model = Producto
+    template_name = "productos/producto_detail.html"
+    context_object_name = "productos"
+
+class ProductoCreateView(CreateView):
+    model = Producto
+    template_name = "productos/producto_form.html"
+    fields = ["nombre", "descripcion", "precio", "stock", "imagen"]
+    success_url = reverse_lazy("lista_productos")
+
+class ProductoUpdateView(UpdateView):
+    model = Producto
+    template_name = "productos/producto_form.html"
+    fields = ["nombre", "descripcion", "precio", "stock", "imagen"]
+    success_url = reverse_lazy("lista_productos")
+
+class ProductoDeleteView(DeleteView):
+    model = Producto
+    template_name = "productos/producto_confirm.html"
+    success_url = reverse_lazy("lista_productos")
+#=== FIN DEL C.R.U.D === 
+
+def desafio(request):
+    empleados =[
+        "Ana Martínez",
+        "Carlos Gómez",
+        "Lucía Fernández",
+        "Juan Torres",
+        "María Pérez",
+        "Pedro Sánchez",
+        "Valeria Ríos",
+        "Esteban Morales",
+        "Sofía Navarro",
+        "Diego Herrera",
+    ]
+    return render(request, "desafio.html", {"empleados":empleados})
